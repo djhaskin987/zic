ad8d2d3de22b37b36ed3d599168f70fa8e73b1dc
Daniel Jay Haskin <djhaskin987@gmail.com>
2020-12-26 14:10:21 -0700

    Changes to get past the certificate errors in testing

--- test.sh
+++ test.sh
@@ -1,3 +1,20 @@
+
 #!/bin/sh
 
-java -jar target/uberjar/zic-0.1.0-SNAPSHOT-standalone.jar add --json-download-authorizations '{"djhaskin987.me": {"type": "basic", "username": "mode", "password": "code"}}' --set-package-name 'a' --set-package-version 0.1.0 --set-package-location "https://djhaskin987.me:8443/a.zip"
+# https://docs.oracle.com/javadb/10.8.3.0/adminguide/cadminsslclient.html
+# https://stackoverflow.com/questions/65376092/can-jvm-trust-a-self-signed-certificate-for-only-a-single-run
+# This was run to create keystore
+# keytool -import -file lighttpd-environment/server.crt -keystore test.keystore
+# where server.crt was just the certificate half of the server PEM file
+# password 'asdfasdf'
+
+
+java -jar \
+    -Djavax.net.ssl.trustStore="test.keystore" \
+    -Djavax.net.ssl.trustStorePassword="asdfasdf" \
+    target/uberjar/zic-0.1.0-SNAPSHOT-standalone.jar \
+    add \
+    --json-download-authorizations '{"djhaskin987.me": {"type": "basic", "username": "mode", "password": "code"}}' \
+    --set-package-name 'a' \
+    --set-package-version 0.1.0 \
+    --set-package-location "https://djhaskin987.me:8443/a.zip"
